ae3f390adfb8115510b9bb035fb246a6a1e5927b
Hemanshu Varma <hemanshuvarma916@gmail.com>
2024-11-10 01:29:38 +0530

    Add: Material Appbar and Navigation controller

--- app/src/main/java/com/varma/hemanshu/a_one/home/HomeFragment.kt
+++ app/src/main/java/com/varma/hemanshu/a_one/home/HomeFragment.kt
@@ -1,27 +1,27 @@
 package com.varma.hemanshu.a_one.home
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
-import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import com.varma.hemanshu.a_one.R
 import com.varma.hemanshu.a_one.databinding.FragmentHomeBinding
+
+private const val TAG = "HomeFragment"
 
 class HomeFragment : Fragment() {
 
     private lateinit var binding: FragmentHomeBinding
     private val homeViewModel: HomeViewModel by viewModels()
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
 
         binding = FragmentHomeBinding.inflate(inflater, container, false)
         binding.viewModel = homeViewModel
@@ -29,8 +29,15 @@ class HomeFragment : Fragment() {
         return binding.root
     }
 
-//    override fun onResume() {
-//        super.onResume()
-//        view?.findNavController()?.navigate(R.id.details)
-//    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setClickListeners()
+    }
+
+    private fun setClickListeners() {
+        binding.btnDetailScreen.setOnClickListener {
+            Log.d(TAG, "Nav to detail screen")
+            findNavController().navigate(R.id.action_home_to_details)
+        }
+    }
 }
