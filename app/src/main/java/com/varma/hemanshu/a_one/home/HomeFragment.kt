0fdda5fa96054df743542244977b42cf6f715028
Hemanshu Varma <hemanshuvarma916@gmail.com>
2024-11-10 13:55:50 +0530

    Update: RecyclerView binding with Adapter/User data

--- app/src/main/java/com/varma/hemanshu/a_one/home/HomeFragment.kt
+++ app/src/main/java/com/varma/hemanshu/a_one/home/HomeFragment.kt
@@ -8,8 +8,12 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.ListAdapter
 import com.varma.hemanshu.a_one.R
+import com.varma.hemanshu.a_one.adapter.UserListAdapter
 import com.varma.hemanshu.a_one.databinding.FragmentHomeBinding
+import com.varma.hemanshu.a_one.models.User
 
 private const val TAG = "HomeFragment"
 
@@ -17,6 +21,7 @@ class HomeFragment : Fragment() {
 
     private lateinit var binding: FragmentHomeBinding
     private val homeViewModel: HomeViewModel by viewModels()
+    private lateinit var userAdapter: UserListAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -32,6 +37,12 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setClickListeners()
+        binding.rvUsers.layoutManager = LinearLayoutManager(context)
+        userAdapter = UserListAdapter { userItem ->
+            Log.d(TAG, "Item clicked: ${userItem.id}")
+        }
+        binding.rvUsers.adapter = userAdapter
+        setObservers()
     }
 
     private fun setClickListeners() {
@@ -39,5 +50,25 @@ class HomeFragment : Fragment() {
             Log.d(TAG, "Nav to detail screen")
             findNavController().navigate(R.id.action_home_to_details)
         }*/
+    }
+
+    private fun setObservers() {
+        //Get data from ViewModel/Repo
+        val usersList = mutableListOf(
+            User(id = 1, name = "Hemanshu Varma", description = "Software Engineer"),
+            User(id = 2, name = "John", description = "SSE"),
+            User(id = 3, name = "Hemanshu", description = "Software Engineer"),
+            User(id = 4, name = "Hemanshu Varma", description = "Software Engineer"),
+            User(id = 5, name = "Hemanshu Varma", description = "Software Engineer"),
+            User(id = 6, name = "Hemanshu Varma", description = "Software Engineer"),
+            User(id = 7, name = "Hemanshu Varma", description = "Software Engineer"),
+            User(id = 8, name = "Hemanshu Varma", description = "Software Engineer"),
+            User(id = 9, name = "Hemanshu Varma", description = "Software Engineer"),
+            User(id = 10, name = "Hemanshu Varma", description = "Software Engineer"),
+            User(id = 11, name = "Hemanshu Varma", description = "Software Engineer"),
+            User(id = 12, name = "Hemanshu Varma", description = "Software Engineer"),
+
+            )
+        userAdapter.submitList(usersList)
     }
 }
